53e1df8e75baf1d69c98632475602ee8c12d88bf
mayangmonika <141295107+mayangmonika@users.noreply.github.com>
2023-08-03 15:00:07 +0700

    Add files via upload

--- CalculateMoneySpent.java
+++ CalculateMoneySpent.java
@@ -1,61 +1,46 @@
 import java.util.Scanner;
 
 public class CalculateMoneySpent{
-	public static void main(String[] args){
-		String name;
-		int moneySpent;
-		double average;
-		int total;
-		int totalDay = 7;
-		
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println("Calculate Your Money!!!\n");
-		
-		System.out.println("Hey, What is your name?");
-		name = input.nextLine();
-		
-		System.out.println("How much money did you spend at the club on Monday");
-		moneySpent = input.nextlnt();
-		
-		total = moneySpent;
-		
-		System.out.println(How much money did you spend at the club on Tuesday");
-		moneySpent = input.nextlnt();
-		
-		total = total + moneySpent; 
-		
-		
-	}
-}
-        total = total + moneySpent;
-		
-		System.out.println("How much money did you spend at the club on Thursday");
-		moneySpent = input.nextlnt();
-		
-		total = moneySpent;
-		
-		System.out.println("How much money did you spend at the club on Friday");
-		moneySpent = input.nextlnt();
-		
-		total = moneySpent;
-		
-		System.out.println("How much money did you spend at the club on Saturday");
-		moneySpent = input.nextlnt();
-		
-		total = moneySpent;
-		
-		System.out.println("How much money did you spend at the club on ");
-		moneySpent = input.nextlnt();
-		
-		total = moneySpent;
-		
-		average = (double) total/totalDay;
-		
-		System.out.prlntn("The Calculation Result:");
-		
-		System.out.prlntn("Hi Jonson.");
-        System.out.prlntn("Your total expediture at the clubthis week is $ " + total);
-        System.out.prlntn("With Ian Average daily expediture of $ " + average)l
-    }	
+	public static void main( String[] args){
+        String name ;
+        int moneySpent , total , totalDay ;
+        double average ;
+
+		Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Hey. What is your name? ");
+        name = keyboard.next();
+
+        System.out.println("How much money did you spent at the club on Monday? ");
+		moneySpent = keyboard.nextInt();
+
+        System.out.println("How much money did you spent at the club on Tuesday? ");
+		moneySpent = moneySpent + keyboard.nextInt();
+
+        System.out.println("How much money did you spent at the club on Wednesday? ");
+		moneySpent = moneySpent + keyboard.nextInt();
+
+        System.out.println("How much money did you spent at the club on Thursday? ");
+		moneySpent = moneySpent + keyboard.nextInt();
+
+        System.out.println("How much money did you spent at the club on Friday? ");
+		moneySpent = moneySpent + keyboard.nextInt();
+
+        System.out.println("How much money did you spent at the club on Saturday? ");
+		moneySpent = moneySpent + keyboard.nextInt();
+
+        System.out.println("How much money did you spent at the club on Sunday? ");
+		moneySpent = moneySpent + keyboard.nextInt();
+
+        System.out.println("The Calculation Results : ");
+        System.out.println("Hi " + name + ". ");
+        // total = moneySpent;
+        // boleh dipake buat penjumlahan atau engga
+        totalDay =(7);
+        average = moneySpent / totalDay;
+        System.out.println("Your total exoenditure at the club this week is $" + moneySpent );
+        System.out.println("With an average daily expenditure of $" + average);
+
+
+    }
 }
